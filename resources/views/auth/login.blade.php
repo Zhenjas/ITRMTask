bb099ee803f76de56d435f032f3641b7cf2cf35e
Jevgenijs Ivanonoks <jevgenijs.ivanonoks@gmail.com>
2018-07-21 00:11:13 +0100

    Added RememberMe checkbox

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,29 +7,34 @@
         </div>
     </div>
 
-        <div class="form">
-            <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-                <div class="field">
-                    <label for="email">Email<span>*</span></label>
-                    <input type="text" class="input-group" name="email" value="{{ old('email') }}">
-                    <span id="email" class="validity form-control{{ $errors->has('email') ? ' visible' : '' }}">
-                        @if ($errors->has('email'))
-                                {{ $errors->first('email') }}
-                        @endif
-                    </span>
-                </div>
-                <div class="field">
-                    <label for="password">Password<span>*</span></label>
-                    <input type="password" class="input-group" name="password" value="{{ old('password') }}">
-                    <span id="password" class="validity form-control{{ $errors->has('password') ? ' visible' : '' }}">
-                        @if ($errors->has('password'))
-                                {{ $errors->first('password') }}
-                        @endif
-                    </span>
-                </div>
-                <button type="submit" id="submit-button">LogIn</button>
-                </div>
-            </form>
-        </div>
+    <div class="form">
+        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                    @csrf
+            <div class="field">
+                <label for="email">Email<span>*</span></label>
+                <input type="text" class="input-group" name="email" value="{{ old('email') }}">
+                <span id="email" class="validity form-control{{ $errors->has('email') ? ' visible' : '' }}">
+                    @if ($errors->has('email'))
+                            {{ $errors->first('email') }}
+                    @endif
+                </span>
+            </div>
+            <div class="field">
+                <label for="password">Password<span>*</span></label>
+                <input type="password" class="input-group" name="password" value="{{ old('password') }}">
+                <span id="password" class="validity form-control{{ $errors->has('password') ? ' visible' : '' }}">
+                    @if ($errors->has('password'))
+                            {{ $errors->first('password') }}
+                    @endif
+                </span>
+            </div>
+            <div class="field-checkbox">
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> 
+                <label for="remember">{{ __('Remember Me') }}</label>
+            </div>
+            <div>
+                <button type="submit" id="login-button">LogIn</button>
+            </div>
+        </form>
+    </div>
 @stop
